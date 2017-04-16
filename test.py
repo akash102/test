699888bb78780ba9f8a8ae8ec048efe494910804
Aakash Patel <Aakash Patel>
2017-04-16 01:47:45 -0700

    last changein new

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ hungry=raw_input("are you hungry?")
 if hungry=="yupp":
     print "eat puff"
 else:
-    newb=raw_input("maj ma")
-    if newb=="yes":
-        print "jalsa kar"
-        print "jagi ja"
+    print "jalsa kar"
+    print "jagi ja"
 
